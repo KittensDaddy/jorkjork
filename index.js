8be43f3bef43621f2be3187fd305e72e10f5af9b
KittensDaddy <128681551+KittensDaddy@users.noreply.github.com>
2024-11-19 06:25:58 +0700

    Revert "Update index.js"
    This reverts commit af80f4e944b71fd972b7aac35755ec88e8798f63.

--- index.js
+++ index.js
@@ -130,8 +130,10 @@ import('node-fetch').then(fetchModule => {
           ])
           .save(outputPath)
           .outputOptions([
-            '-r 30',               // Frame rate of 25fps
-            '-fs', '15M'           // Limit file size to 27MB
+            '-r 30',               // Frame rate of 30fps
+            '-fs', '15M',           // Limit file size to 15MB
+            '-filter_complex',      // This enables more detailed filtering
+            'fps=30,scale=iw:ih:flags=lanczos' // Apply Lanczos scaling
           ])
           .on('end', () => {
             console.log('Media combined successfully');
@@ -145,7 +147,4 @@ import('node-fetch').then(fetchModule => {
         console.log(ffmpegCommand._getArguments().join(' '));
       });
     });
-  };
-}).catch(err => {
-  console.error('Error loading node-fetch:', err);
-});
+};
